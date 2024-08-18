e42ccb7b8f9561706670dd48c570b938565d737f
Dineshyadav3505 <dineshyadav20001210@gmail.com>
2024-08-18 22:41:22 +0530

    Add @tabler/icons-react and react-dropzone dependencies

--- src/app/[name]/page.tsx
+++ src/app/[name]/page.tsx
@@ -1,38 +1,33 @@
 'use client';
 
-import React, { useEffect, useState } from 'react';
-import { useRouter } from 'next/router';
-
-interface QueryParams {
-  h1: string;
-  h2: string;
-  api: string;
-}
+import React, { useState } from "react";
+import { useSearchParams } from 'next/navigation';
+import { FileUpload } from "@/components/ui/file-upload";
 
 const SamplePage: React.FC = () => {
-  const router = useRouter();
-  const [query, setQuery] = useState<QueryParams | null>(null);
+    const [files, setFiles] = useState<File[]>([]);
+    const handleFileUpload = (files: File[]) => {
+      setFiles(files);
+      console.log(files);
+    };
+  
+  const searchParams = useSearchParams();
 
-  useEffect(() => {
-    if (router.isReady) {
-      const { h1, h2, api } = router.query;
-      setQuery({
-        h1: h1 as string,
-        h2: h2 as string,
-        api: api as string,
-      });
-    }
-  }, [router.isReady]);
+  const h1 = searchParams.get('h1');
+  const h2 = searchParams.get('h2');
+  const api = searchParams.get('api');
 
-  if (!query) {
+  if (!h1 || !h2 || !api) {
     return <div>Loading...</div>; // Show a loading state while the query parameters are being retrieved
   }
 
   return (
-    <div className="p-5">
-      <h1 className="text-2xl font-bold">{query.h1}</h1>
-      <h2 className="text-xl">{query.h2}</h2>
-      <p className="text-lg">API: {query.api}</p>
+    <div className="p-5 lg:px-10 text-white min-h-[30rem] py-10 ">
+        <div className=" bg-zinc-950 shadow-lg rounded-2xl w-full flex flex-col items-center justify-center ">
+            <h1 className=" text-xl lg:text-5xl font-bold">{h1}</h1>
+            <h2 className="text-sm lg:text-xl mb-10">{h2}</h2>
+            <FileUpload onChange={handleFileUpload} />
+        </div>
     </div>
   );
 };
